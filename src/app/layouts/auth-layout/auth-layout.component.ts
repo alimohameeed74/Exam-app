b66669b096154bf6fbe7f2712731f8f5527c1581
aliimohamed <aliimohamed2003@gmail.com>
2026-07-12 14:16:06 +0300

    init login branch and put base code

--- src/app/layouts/auth-layout/auth-layout.component.ts
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-auth-layout',
   templateUrl: './auth-layout.component.html',
-  styleUrls: ['./auth-layout.component.css']
+  styleUrls: ['./auth-layout.component.css'],
+  imports: [RouterOutlet],
 })
 export class AuthLayoutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
