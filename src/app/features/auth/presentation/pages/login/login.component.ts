697ada4b6aca25b9d851618e891a80d4734ed4f9
aliimohamed <aliimohamed2003@gmail.com>
2026-07-21 18:26:29 +0300

    working on login feature

--- src/app/features/auth/presentation/pages/login/login.component.ts
+++ src/app/features/auth/presentation/pages/login/login.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedInputComponent } from '../../../../../shared/components/shared-input/shared-input.component';
+import { ErrorMessComponent } from '../../../../../shared/components/error-mess/error-mess.component';
+import { AlertComponent } from '../../../../../shared/components/alert/alert.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  imports: [SharedInputComponent, ErrorMessComponent, AlertComponent, RouterLink],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
