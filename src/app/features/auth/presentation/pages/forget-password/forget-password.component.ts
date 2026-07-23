d57e6f09b1049d7728e06946d8daef9047688d93
aliimohamed <aliimohamed2003@gmail.com>
2026-07-23 14:52:33 +0300

    working on forget-password

--- src/app/features/auth/presentation/pages/forget-password/forget-password.component.ts
+++ src/app/features/auth/presentation/pages/forget-password/forget-password.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal, WritableSignal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { SharedInputComponent } from '../../../../../shared/components/shared-input/shared-input.component';
+import { FormBuilder, Validators } from '@angular/forms';
+import { email } from '@angular/forms/signals';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-forget-password',
   templateUrl: './forget-password.component.html',
-  styleUrls: ['./forget-password.component.css']
+  styleUrls: ['./forget-password.component.css'],
+  imports: [RouterLink, SharedInputComponent],
 })
-export class ForgetPasswordComponent implements OnInit {
+export class ForgetPasswordComponent implements OnInit, OnDestroy {
+  private fb = inject(FormBuilder);
+  private destroy$ = new Subject<void>();
+  step: WritableSignal<number> = signal(3);
+  emailForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+  });
+  passwordsForm = this.fb.group({
+    newPassword: ['', [Validators.required]],
+    confirmPassword: ['', [Validators.required]],
+  });
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  icreaseStep() {}
+
+  get emailController() {
+    return this.emailForm.controls.email;
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next();
+  }
 }
