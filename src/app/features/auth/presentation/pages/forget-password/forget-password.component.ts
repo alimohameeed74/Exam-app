bd38df5d60b2e317d05d44f6bfd8cf38aef4de7f
aliimohamed <aliimohamed2003@gmail.com>
2026-07-23 15:32:19 +0300

    updated

--- src/app/features/auth/presentation/pages/forget-password/forget-password.component.ts
+++ src/app/features/auth/presentation/pages/forget-password/forget-password.component.ts
@@ -24,7 +24,6 @@ export class ForgetPasswordComponent implements OnInit, OnDestroy {
   step: WritableSignal<number> = signal(1);
   emailForm = this.fb.nonNullable.group({
     email: ['', [Validators.required, Validators.email]],
-    redirectUrl: ['/auth'],
   });
   passwordsForm = this.fb.group(
     {
