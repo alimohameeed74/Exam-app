88963bd637942aa96ddd3ba13900a0b98b6094b8
aliimohamed <aliimohamed2003@gmail.com>
2026-09-07 16:30:38 +0300

    testing account service

--- src/app/core/guards/auth-guard/auth-guard.spec.ts
+++ src/app/core/guards/auth-guard/auth-guard.spec.ts
@@ -1,7 +1,6 @@
+import { authGuard } from './auth.guard';
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
-
-import { authGuard } from './auth-guard';
 
 describe('authGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) =>
